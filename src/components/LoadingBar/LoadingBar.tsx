ccddee53922aea199183aa86ee67cfeee9ce1f6f
Mohsen Malekan <mhs.malekan@gmail.com>
2019-09-12 13:34:41 +0430

    chnge LoadingBar color depend on theme.type

--- src/components/LoadingBar/LoadingBar.tsx
+++ src/components/LoadingBar/LoadingBar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useTheme } from '@material-ui/styles';
-import { Theme } from '@material-ui/core';
+import React from "react";
+import { useTheme } from "@material-ui/styles";
+import { Theme } from "@material-ui/core";
 import TopLoadingBar from "react-top-loading-bar";
 import { useLoadingBarStore } from "components/LoadingBar";
 
@@ -12,9 +12,13 @@ const LoadingBar = () => {
     <TopLoadingBar
       progress={state.progress}
       height={3}
-      color={theme.palette.primary.main}
+      color={
+        theme.palette.type === "light"
+          ? theme.palette.primary.main
+          : theme.palette.secondary.main
+      }
     />
   );
-}
+};
 
 export default LoadingBar;
