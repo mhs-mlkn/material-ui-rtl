a2ce7dc79e9e8bdd1f46905179c51143b5c580c5
Mohsen Malekan <mhs.malekan@gmail.com>
2019-09-12 16:57:57 +0430

    add router interceptor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,31 @@ import React from "react";
 import Snackbar from "components/CustomSnackbar";
 import Theme from "components/Theme";
 import LoadingBar, { useLoadingBarStore } from "components/LoadingBar";
+import { useInterceptor, useRoutes, navigate } from "hookrouter";
+
+const interceptFunction = (currentPath: any, nextPath: any) => {
+  if (window.confirm("Do you want to leave?")) {
+    return nextPath;
+  }
+  return currentPath;
+};
+
+const routes = {
+  "/": () => <h1>HOME</h1>,
+  "/about": () => <h1>ABOUT</h1>
+};
 
 const TestSnackbar: React.FC = () => {
   const loadingActions = useLoadingBarStore()[1];
 
   const startLoadingBar = () => {
     loadingActions.start();
+    navigate("/");
   };
 
   const completeLoadingBar = () => {
     loadingActions.complete();
+    navigate("/about");
   };
 
   return (
@@ -23,11 +38,15 @@ const TestSnackbar: React.FC = () => {
 };
 
 const App: React.FC = () => {
+  useInterceptor(interceptFunction);
+  const routeResult = useRoutes(routes);
+
   return (
     <Theme>
       <Snackbar>
         <LoadingBar />
         <TestSnackbar />
+        {routeResult || <h1>NOT FOUND</h1>}
       </Snackbar>
     </Theme>
   );
