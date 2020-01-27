86e7c7b14d851c4cead18195e9f2415a0d12c90c
Mohsen-Malekan <mhs.malekan@gmail.com>
2020-01-27 15:36:45 +0330

    DashboardSelect

--- src/components/Dashboard/DashboardSelect.tsx
+++ src/components/Dashboard/DashboardSelect.tsx
@@ -1,48 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import TextField from "@material-ui/core/TextField";
 import Tooltip from "@material-ui/core/Tooltip";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import {
-  TDashboard,
-  useDashboards,
-  DashboardsService as Service
-} from "components/Dashboard";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import { TDashboard, DashboardMenu } from "components/Dashboard";
 
 const DashboardSelect = () => {
   const history = useHistory();
-  const [state] = useDashboards();
-  const [selected, setSelected] = useState<undefined | TDashboard>();
+  const [selected, setSelected] = useState("");
   const selectedId = window.location.pathname.split("/").pop() || 0;
 
-  useEffect(() => {
-    const dashboard = Service.get(+selectedId);
-    setSelected(dashboard);
-  }, [state.dashboards, selectedId]);
-
   const handleChange = (dashboard: TDashboard) => {
-    // setName(dashboard.name);
+    setSelected(dashboard.name);
     history.push(`/user/dashboard/${dashboard.id}`, { title: dashboard.name });
   };
 
   return (
-    <Tooltip title={!!selected ? selected.name : ""}>
-      <Autocomplete
-        options={state.dashboards}
-        getOptionLabel={(d: TDashboard) => d.name}
-        disableClearable
-        multiple={false}
+    <Tooltip title={selected}>
+      <TextField
+        variant="outlined"
+        margin="dense"
+        disabled
+        style={{ minWidth: 550 }}
         value={selected}
-        getOptionSelected={option => option === selected}
-        renderInput={params => (
-          <TextField
-            {...params}
-            variant="outlined"
-            margin="dense"
-            value={selected}
-            style={{ minWidth: 550 }}
-          />
-        )}
+        inputProps={{
+          readOnly: true
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <DashboardMenu selectedId={+selectedId} onChange={handleChange} />
+            </InputAdornment>
+          )
+        }}
       />
     </Tooltip>
   );
